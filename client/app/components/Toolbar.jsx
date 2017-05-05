168489a2b1c93cabe009aeaa09247ad736982fe8
Michael Robinson <mike@pagesofinterest.net>
2017-05-06 02:38:20 +1200

    (toolbar) Connect toolbar to redux, add "load more comics" button

--- client/app/components/Toolbar.jsx
+++ client/app/components/Toolbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Toolbar.pcss'
 import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import pathOr from 'ramda/src/pathOr'
+import { loadMoreComics } from './comics/actions'
 
-export default class Toolbar extends React.Component {
+function ComicControls(props) {
+  return (
+    <div className="toolbar__controls">
+      <button onClick={() => props.dispatch(loadMoreComics())}>
+        More Comics
+      </button>
+    </div>
+  )
+}
+
+class Toolbar extends React.Component {
   render () {
+    const showComicControls = '/comics' === pathOr('', ['routing', 'locationBeforeTransitions', 'pathname'], this.props)
     return (
       <div className="toolbar">
         <h1 className="logo">
@@ -13,17 +27,28 @@ export default class Toolbar extends React.Component {
           </Link>
         </h1>
         <div className="toolbar__links">
-          <Link to="/comics" activeClassName="toolbar__links--active">
+          <Link to="/comics"
+                activeClassName="toolbar__links--active">
             Comics
           </Link>
-          <Link to="/creators" activeClassName="toolbar__links--active">
+          <Link to="/creators"
+                activeClassName="toolbar__links--active">
             Creators
           </Link>
-          <Link to="/characters" activeClassName="toolbar__links--active">
+          <Link to="/characters"
+                activeClassName="toolbar__links--active">
             Characters
           </Link>
         </div>
+
+        {showComicControls && <ComicControls {...this.props} />}
       </div>
     )
   }
 }
+
+function mapStateToProps (state) {
+  return {routing: state.routing}
+}
+
+export default connect(mapStateToProps)(Toolbar)
