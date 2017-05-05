cd26662c9c94ada98dbf7bae98fccdc39b27b5a2
Michael Robinson <mike@pagesofinterest.net>
2017-05-05 15:20:56 +1200

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,4 +6,6 @@ set -x
 export NODE_ENV="production"
 pwd
 ls -lha /var/app/current
+ls -lha /var/app/current/server
+
 /var/app/current/server/node_modules/babel-cli/bin/babel-node.js /var/app/current/server/index.js | /var/app/current/server/node_modules/bunyan/bin/bunyan
