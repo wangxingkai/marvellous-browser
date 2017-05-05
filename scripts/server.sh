775a8a081a426927129c8ff0c2ed80bd287c0b85
Michael Robinson <mike@pagesofinterest.net>
2017-05-05 12:25:06 +1200

    (build) Do not rely on symlinks in prod

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,5 +5,4 @@ set -x
 
 export NODE_ENV="production"
 
-cd server
-./node_modules/.bin/babel-node ./index.js | ./node_modules/.bin/bunyan
+server/node_modules/babel-cli/bin/babel-node.js server/index.js | server/node_modules/bunyan/bin/bunyan
