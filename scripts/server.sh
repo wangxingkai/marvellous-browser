dc137f91f7fc5dde616a500983ce1ae3fcf489c6
Michael Robinson <mike@pagesofinterest.net>
2017-05-05 13:24:21 +1200

    (build) Update server start script

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 export NODE_ENV="production"
-
-server/node_modules/babel-cli/bin/babel-node.js server/index.js | server/node_modules/bunyan/bin/bunyan
+pwd
+/var/app/current/server/node_modules/babel-cli/bin/babel-node.js /var/app/current/server/index.js | /var/app/current/server/node_modules/bunyan/bin/bunyan
