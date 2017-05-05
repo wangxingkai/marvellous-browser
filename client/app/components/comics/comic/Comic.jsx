fa9a3ca1a0fa7dccf4bf87395420bf6243ad1d8c
Michael Robinson <mike@pagesofinterest.net>
2017-05-06 02:37:33 +1200

    (client) Comic detail page

--- client/app/components/comics/comic/Comic.jsx
+++ client/app/components/comics/comic/Comic.jsx
@@ -2,27 +2,75 @@ import React from 'react'
 import { gql, graphql } from 'react-apollo'
 import pathOr from 'ramda/src/pathOr'
 import './Comic.pcss'
+import head from 'ramda/src/head'
+import length from 'ramda/src/length'
+
+function Characters (props) {
+  if (!length(props.characters)) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>Characters</h2>
+      <div className="comic__characters">
+        {props.characters.map((character) => {
+          return (
+            <div key={character.id}
+                 className="comic__characters__character">
+              <h3 >
+                {character.name} {character.role && `(${character.role})`}
+              </h3>
+              {character.description && <p>{character.description}</p>}
+              <img src={character.thumbnail}/>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
 
 class ComicRenderer extends React.Component {
   render () {
-    const comic = pathOr([], ['data', 'comics'])(this.props)
-    console.log(comic)
+    const comic = pathOr(false, ['data', 'comic'])(this.props)
+
+    if (!comic) {
+      return null
+    }
 
     return (
       <div className="comic">
+        <img className="comic__hero"
+             src={head(comic.images)}/>
+        <div className="comic__body">
+          <h1>{comic.title}</h1>
+          <p>{comic.description}</p>
+
+          <Characters characters={comic.characters}/>
+        </div>
       </div>
     )
   }
 }
 
 const Comic = graphql(gql`
-        query ($id: Int) {
-            comic(id:$id){
-            id
-            title
-          }
-        }
-        `, {
+      query ($id: Int!) {
+      comic(id:$id){
+      id
+      title
+      images
+      description
+      characters {
+      id
+      description
+      name
+      role
+      thumbnail
+    }
+    }
+    }
+      `, {
   options: {
     notifyOnNetworkStatusChange: true
   }
