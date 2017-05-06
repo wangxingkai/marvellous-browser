71b3cd523174f9ef6136c4c97ffcaf203487cecc
Michael Robinson <mike@pagesofinterest.net>
2017-05-06 23:58:06 +1200

    (test) Added test scripts

---
+++ scripts/test-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PORT=9999
+
+cd server
+node_modules/.bin/lab -T node_modules/lab-babel \
+    --verbose \
+    -t 100 \
+    -S \
+    --timeout 10000 \
+    --ignore '__core-js_shared__,core,System,asap,Observable,regeneratorRuntime,_babelPolyfill'
+
