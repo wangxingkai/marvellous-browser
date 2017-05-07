6318f501e44467f942cf2921dd1394770084d837
Michael Robinson <mike@pagesofinterest.net>
2017-05-07 13:44:15 +1200

    (client) Added redux-thunk, redux-multi & react-router-scroll

--- client/app/bootstrap.jsx
+++ client/app/bootstrap.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 import { ApolloProvider } from 'react-apollo'
-import { browserHistory, Route, Router } from 'react-router'
+import { applyRouterMiddleware, browserHistory, Route, Router } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import { store } from './store'
 import Comics from './components/comics/components/Comics'
 import Comic from './components/comics/components/comic/Comic'
 import { client } from './client'
+import useScroll from 'react-router-scroll/lib/useScroll'
 
 ReactDOM.render(
   <ApolloProvider client={client}
                   store={store}>
-    <Router history={syncHistoryWithStore(browserHistory, store)}>
+    <Router
+      render={applyRouterMiddleware(useScroll())}
+      history={syncHistoryWithStore(browserHistory, store)}>
       <Route path="/"
              component={App}>
         <Route path="/comics"
+               ignoreScrollBehavior
                component={Comics}>
           <Route path="/comics/:id"
-                 component={(props) => <Comic id={props.params.id} />}/>
+                 component={(props) => <Comic id={props.params.id}/>}/>
         </Route>
       </Route>
     </Router>
   </ApolloProvider>,
-  document.getElementById('root'))
+  document.getElementById('root')
+)
--- client/app/store.js
+++ client/app/store.js
@@ -5,8 +5,14 @@ import { routerReducer } from 'react-router-redux'
 import Progress from 'react-progress-2'
 import { comics } from './components/comics/reducers'
 import { toolbar } from './components/toolbar/reducers'
+import thunkMiddleware from 'redux-thunk'
+import multi from 'redux-multi'
 
-const middleware = [client.middleware()]
+const middleware = [
+  client.middleware(),
+  thunkMiddleware,
+  multi
+]
 
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(require('redux-freeze'))
@@ -24,7 +30,8 @@ export const store = createStore(
   {},
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() :
+      f => f
   )
 )
 
