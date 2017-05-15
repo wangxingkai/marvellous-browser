127302816313aeb15002fb96886b6bd7ca41325a
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-16 08:47:47 +1200

    (comic) Made comic dataloader very basic

--- server/schema/dataloaders/comic.js
+++ server/schema/dataloaders/comic.js
@@ -1,24 +1,13 @@
-import { marvel } from '../../marvel'
+import {marvel} from '../../marvel'
 import DataLoader from 'dataloader'
-import { transformComic } from '../transformers/comic'
+import {transformComic} from '../transformers/comic'
 import R from 'ramda'
-import { characterLoader, extractCharacterIdFromURI } from './character'
 
 const getData = R.pathOr({}, ['data', 0])
-const getCharacterId = R.compose(
-  extractCharacterIdFromURI,
-  R.prop('resourceURI')
-)
-
 export const comicLoader = new DataLoader(async (ids) => {
   return Promise.all(R.map(async (id) => {
     try {
-      const response = await marvel.comics.find(id)
-      const comic = transformComic(getData(response))
-
-      comic.characters = await characterLoader.loadMany(R.map(getCharacterId, comic.characters))
-
-      return comic
+      return transformComic(getData(await marvel.comics.find(id)))
     } catch (error) {
       console.error(error)
       throw new Error(`Failed to fetch comic with id ${id}`)
