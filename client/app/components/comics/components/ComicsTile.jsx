c3c907825a2a2806e6c5aba906252c9afa2132f0
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-16 08:44:08 +1200

    (comics) Made ComicsTile.jsx stateless

--- client/app/components/comics/components/ComicsTile.jsx
+++ client/app/components/comics/components/ComicsTile.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './ComicsTile.pcss'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 import classNames from 'classnames'
-import { getColourForComicId } from './comicTileColours'
+import {getColourForKey} from '../../../random-colours'
 import ifElse from 'ramda/src/ifElse'
 import assoc from 'ramda/src/assoc'
 import prop from 'ramda/src/prop'
@@ -10,29 +10,26 @@ import prop from 'ramda/src/prop'
 const styleForComic = ifElse(
   prop('hasImages'),
   (comic) => assoc('backgroundImage', `url(${prop('thumbnail', comic)})`, {}),
-  (comic) => assoc('backgroundColor', getColourForComicId(prop('id', comic)), {})
+  (comic) => assoc('backgroundColor', getColourForKey(`comic-${prop('id', comic)}`), {})
 )
 
-export default class ComicTile extends React.Component {
+export default function ComicTile(props) {
+  const comic = props.comic
 
-  render () {
-    const comic = this.props.comic
+  const comicClass = classNames({
+    'comic-tile': true,
+    'comic-tile--no-images': !comic.hasImages
+  })
 
-    const comicClass = classNames({
-      'comic-tile': true,
-      'comic-tile--no-images': !comic.hasImages
-    })
-
-    return (
-      <Link to={`/comics/${comic.id}`}
-            className={comicClass}
-            style={styleForComic(comic)}>
-        {comic.hasImages && <img src={comic.thumbnail}/>}
-        <div className="comic-tile__detail">
-          <h3>{comic.title}</h3>
-        </div>
-      </Link>
-    )
-  }
+  return (
+    <Link to={`/comics/${comic.id}`}
+          className={comicClass}
+          style={styleForComic(comic)}>
+      {comic.hasImages && <img src={comic.thumbnail}/>}
+      <div className="comic-tile__detail">
+        <h3>{comic.title}</h3>
+      </div>
+    </Link>
+  )
 }
 
