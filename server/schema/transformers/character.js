6e58055718f27e16a5ac92073a7337f851eefc45
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-16 09:51:55 +1200

    (character) Updated comicsTotal extraction

--- server/schema/transformers/character.js
+++ server/schema/transformers/character.js
@@ -9,7 +9,13 @@ export const transformCharacter = (rawCharacter) => {
       comics: R.prop('items'),
       description: cleanString
     }),
-    (rawCharacterArg) => R.assoc('comicsTotal', R.pathOr(0, ['comics', 'available'], rawCharacterArg), rawCharacterArg),
+    R.converge(
+      R.assoc('comicsTotal'),
+      [
+        R.pathOr(0, ['comics', 'available']),
+        R.identity
+      ]
+    ),
     R.pick([
       'id',
       'name',
