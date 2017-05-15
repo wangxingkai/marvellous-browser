670c19dc71676db3b694de2fbb1fbcd46e3f8443
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-16 09:07:03 +1200

    (characters) Updated cahracters loaders

--- server/schema/dataloaders/character.js
+++ server/schema/dataloaders/character.js
@@ -1,16 +1,14 @@
 import DataLoader from 'dataloader'
-import { marvel } from '../../marvel'
+import {marvel} from '../../marvel'
 import R from 'ramda'
-import { transformCharacter } from '../transformers/character'
+import {transformCharacter} from '../transformers/character'
 
-export const extractCharacterIdFromURI = R.compose(R.last, R.split('/'))
 const getData = R.pathOr({}, ['data', 0])
 
 export const characterLoader = new DataLoader(async (ids) => {
   return Promise.all(R.map(async (id) => {
     try {
-      const characterRawDetail = await marvel.characters.find(id)
-      return transformCharacter(getData(characterRawDetail))
+      return transformCharacter(getData(await marvel.characters.find(id)))
     } catch (error) {
       console.error(error)
       throw new Error(`Failed to fetch character with id ${id}`)
--- server/schema/transformers/character.js
+++ server/schema/transformers/character.js
@@ -1,18 +1,23 @@
 import R from 'ramda'
-import { transformImage } from './images'
+import {transformImage} from './images'
+import {cleanString} from '../../helpers/clean-string'
 
 export const transformCharacter = (rawCharacter) => {
   return R.compose(
     R.evolve({
-      thumbnail: transformImage
+      thumbnail: transformImage,
+      comics: R.prop('items'),
+      description: cleanString
     }),
+    (rawCharacterArg) => R.assoc('comicsTotal', R.pathOr(0, ['comics', 'available'], rawCharacterArg), rawCharacterArg),
     R.pick([
       'id',
       'name',
       'role',
       'description',
       'thumbnail',
-      'images'
+      'images',
+      'comics'
     ])
   )(rawCharacter)
 }
