68a9732855ed920fcd061ec1e7c6a4c3785ee4ea
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-16 09:05:26 +1200

    (resolvers) Added character resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,22 +1,27 @@
-import { comicLoader } from './dataloaders/comic'
-import { comicsLoader } from './dataloaders/comics'
-import { COMICS_ORDER_ISSUE_NUMBER_DESC } from '../constants'
+import {comicDetailLoader} from './dataloaders/comicDetail'
+import {comicsLoader} from './dataloaders/comics'
+import {COMICS_ORDER_ISSUE_NUMBER_DESC} from '../constants'
+import {characterDetailLoader} from './dataloaders/characterDetail'
+import {charactersLoader} from './dataloaders/characters'
+import R from 'ramda'
 
 export const resolvers = {
   Query: {
-    async comic(
+    comic(
       obj,
       {id}
     ) {
-      return comicLoader.load(id)
+      return comicDetailLoader.load(id)
     },
+
     comics(
       obj,
       {
         start,
         limit,
         orderBy = COMICS_ORDER_ISSUE_NUMBER_DESC,
-        titleStartsWith = false
+        titleStartsWith = false,
+        characterIds = false
       }
     ) {
       const keys = {
@@ -29,7 +34,40 @@ export const resolvers = {
         keys.titleStartsWith = titleStartsWith
       }
 
+      if (characterIds && R.length(characterIds)) {
+        keys.characters = R.join(',', characterIds)
+      }
+
       return comicsLoader.load(JSON.stringify(keys))
+    },
+
+    character(
+      obj,
+      {id}
+    ) {
+      return characterDetailLoader.load(id)
+    },
+
+    characters(
+      obj,
+      {
+        start,
+        limit,
+        orderBy,
+        nameStartsWith = false
+      }
+    ) {
+      const keys = {
+        limit: limit || 12,
+        offset: start || 0,
+        orderBy
+      }
+
+      if (nameStartsWith) {
+        keys.nameStartsWith = nameStartsWith
+      }
+
+      return charactersLoader.load(JSON.stringify(keys))
     }
   }
 }
