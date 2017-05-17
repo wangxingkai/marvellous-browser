aa7d4b64c9476e9a31f1bda63c837e543fe68c3a
Michael Robinson <michael.robinson@solnet.co.nz>
2017-05-17 16:34:30 +1200

    (prerender) Set base href properly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
       inject: 'body',
       cache: false,
       locals: {
-        baseHref: 'aslkdjflkasjdlfkjasdlkjf'
+        baseHref: process.env.BASE_HREF
       }
     }),
     new webpack.DefinePlugin({
